f0b3f65c8e00c9bb0e5c432f77cd0d36903059a2
Tomas <tomas.fryza@gmail.com>
2021-11-22 19:25:52 +0100

    [C] Comments and speed test added

--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,8 +1,10 @@
+
+/* Includes ----------------------------------------------------------*/
 #include "settings.h"
 
-#include <avr/io.h>
-#include <util/delay.h>
-#include <avr/interrupt.h>
+#include <util/delay.h>     // Functions for busy-wait delay loops
+#include <avr/io.h>         // AVR device-specific IO definitions
+#include <avr/interrupt.h>  // Interrupts standard C library for AVR-GCC
 
 #include "i2c.h"
 #include "led.h"
@@ -15,11 +17,19 @@
 
 #include "wyk_stdio.h"
 
+/* Function definitions ----------------------------------------------*/
+/**********************************************************************
+ * Function: Main function where the program execution begins
+ * Purpose:  Init communications, set default motor speed to 75%, 
+ *           turn all LEDs, read sensor values, and send them via UART.
+ * Returns:  none
+ **********************************************************************/
 int main(void)
 {
     LSM9DS1 imu;
     LED led;
     Motor motor;
+    uint8_t speed = 75;
 
     // bluetooth_servise();
     usb_init();
@@ -30,14 +40,15 @@ int main(void)
     sei();
 
     // Set default speed
-    motor.forward(50);
+    motor.forward(speed);
 
     // Turn on all LEDs
-    led.forward_left(true);
+    led.forward_left (true);
     led.forward_right(true);
-    led.reverse_left(true);
+    led.reverse_left (true);
     led.reverse_right(true);
 
+    // Init communication with sensors
     if (!imu.begin())
     {
         usb_puts((char *) "Failed to communicate with LSM9DS1.\n");
@@ -45,41 +56,64 @@ int main(void)
     }
     imu.calibrate();
 
+    // Infinite loop
     while (1)
     {
+        // Read values from sensors
         imu.readTemp();
         imu.readMag();
         imu.readGyro();
         imu.readAccel();
 
-        motor.forward(50);
-        led.forward_left(false);
-        led.forward_right(false);
-        led.reverse_left(false);
-        led.reverse_right(false);
-        _delay_ms(500);
+        // Just for testing. Use any terminal, such as PuTTY in 8N1 mode, 38400 Bd
+        usb_printf("deg/s:  %3.0f %3.0f %3.0f  |  ", imu.calcGyro(imu.gx),    imu.calcGyro(imu.gy),    imu.calcGyro(imu.gz));
+        usb_printf("a [g]:  %2.1f %2.1f %2.1f  |  ", imu.calcAccel(imu.ax),   imu.calcAccel(imu.ay),   imu.calcAccel(imu.az));
+        usb_printf("B [uT]: %4.0f %4.0f %4.0f  |  ", imu.calcMag(imu.mx)*100, imu.calcMag(imu.my)*100, imu.calcMag(imu.mz)*100);
+        usb_printf("T [C]:  %2.1f\r\n", 25.0 + ((double) imu.temperature) / 16.0);
 
-        motor.forward(70);
-        led.forward_left(true);
-        led.forward_right(true);
-        led.reverse_left(true);
-        led.reverse_right(true);
-        _delay_ms(500);
-
-        // Just for testing. Use "make miniterm" command or PuTTY in 8N1 mode, 38400 Bd
-        printf("deg/s: %3.0f %3.0f %3.0f  |  ", imu.calcGyro(imu.gx), imu.calcGyro(imu.gy), imu.calcGyro(imu.gz));
-        printf("a [g]: %2.1f %2.1f %2.1f  |  ", imu.calcAccel(imu.ax), imu.calcAccel(imu.ay), imu.calcAccel(imu.az));
-        printf("B [uT]: %4.0f %4.0f %4.0f  |  ", imu.calcMag(imu.mx) * 100, imu.calcMag(imu.my) * 100, imu.calcMag(imu.mz) * 100);
-        printf("T [C]: %2.1f\r\n", 25.0 + ((double) imu.temperature) / 16.0);
+        // Some speed tests:)
+/*        speed++;
+        if (speed >= 95)
+            speed = 40;
+        if (speed > 80) {
+            led.forward_left (true);
+            led.forward_right(true);
+            led.reverse_left (true);
+            led.reverse_right(true);
+        } else if (speed > 60) {
+            led.forward_left (false);
+            led.forward_right(false);
+            led.reverse_left (true);
+            led.reverse_right(true);
+        } else if (speed > 40) {
+            led.forward_left (false);
+            led.forward_right(false);
+            led.reverse_left (false);
+            led.reverse_right(false);
+        }
+        motor.forward(speed);
+        _delay_ms(250);*/
     }
-} // main
 
+    // Will never reach this
+    return 0;
+}
+
+/* Interrupt service routines ----------------------------------------*/
+/**********************************************************************
+ * Function: USART0_RX_vect
+ * Purpose:  TBD.
+ **********************************************************************/
 ISR(BLUETOOTH_RX_vect)
 {
     uint8_t tmp = BLUETOOTH_DATA;
     usb_putc(tmp);
 }
 
+/**********************************************************************
+ * Function: USART1_RX_vect
+ * Purpose:  TBD.
+ **********************************************************************/
 ISR(USB_RX_vect)
 {
     uint8_t tmp = USB_DATA;
